3e8a55ac2e8e137f6ab69d37a2e3291560e66d30
rogermina08 <rogermina629@gmail.com>
2022-06-12 18:47:40 -0500

    Se eliminan algunas carpetas java.

--- Java/tiendavirtualgp2/src/test/java/Paquete1/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package Paquete1;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
